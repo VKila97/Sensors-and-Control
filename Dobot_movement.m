0388a16cb5ef5df30c0bd62baceb9dedd2308571
Jared-Staskun <jared.staskun@gmail.com>
2022-05-03 21:49:51 +1000

    Movement pattern script

--- Dobot_movement.m
+++ Dobot_movement.m
@@ -23,22 +23,49 @@ send(safetyStatePublisher,safetyStateMsg);
 
 %% End Effector Movement Pattern
 
-target = [0,0,0];
+tool_target = [0,0,0];
+[toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
 
-for i = 0; i < 3; i++
-    target(1) = target(1) + 5;
-    %movement function
-    target(2) = target(2) + 5;
-    %movement function
-    target(3) = target(3) + 5;
-    %movement function
-    target(1) = target(1) - 5;
-    %movement function
-    target(2) = target(2) - 5;
-    %movement function
-    target(3) = target(3) - 5;
-    %movement function
+for i = 1:1:3 %Perform pattern 3x
+    %End effector movement
+    tool_target(1) = tool_target(1) + 5;
+    end_effector_target(tool_target);
+    tool_target(2) = tool_target(2) + 5;
+    end_effector_target(tool_target);
+    tool_target(3) = tool_target(3) + 5;
+    end_effector_target(tool_target);
+    
+    %Turn suction on
+    toolStateMsg.Data = [1]; % Send 1 for on and 0 for off 
+    send(toolStatePub,toolStateMsg);
+    
+    %End effector movement
+    tool_target(1) = tool_target(1) - 5;
+    end_effector_target(tool_target);
+    tool_target(2) = tool_target(2) - 5;
+    end_effector_target(tool_target);
+    tool_target(3) = tool_target(3) - 5;
+    end_effector_target(tool_target);
+    
+    %Turn suction off
+    toolStateMsg.Data = [0]; % Send 1 for on and 0 for off 
+    send(toolStatePub,toolStateMsg);
 end
+
 %% End Program
 
 rosshutdown;
+
+%% Functions
+
+function [] = end_effector_target(target)
+% Takes matrix input containing x,y and z coordinates. Moves Dobot end
+% effector to input coordinates.
+    [targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+
+    targetEndEffectorMsg.Position.X = target(1);
+    targetEndEffectorMsg.Position.Y = target(2);
+    targetEndEffectorMsg.Position.Z = target(3);
+
+    send(targetEndEffectorPub,targetEndEffectorMsg);
+end
