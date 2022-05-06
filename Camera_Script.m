4788677f1ccd801862b4ec434ef1f9774bc2fa75
Jared-Staskun <jared.staskun@gmail.com>
2022-05-06 23:38:23 +1000

    more color filtering

--- Camera_Script.m
+++ Camera_Script.m
@@ -19,6 +19,8 @@ sub = rossubscriber("camera/color/image_raw");
 image = readImage(msg2);
 %imshow(image)
 
+%% Red
+
 % Convert RGB image to chosen color space
 I = rgb2hsv(image);
 
@@ -41,7 +43,32 @@ sliderBW = ( (I(:,:,1) >= channel1Min) | (I(:,:,1) <= channel1Max) ) & ...
 BW = sliderBW;
 imshow(BW)
 
+%% Green
 
+
+% Convert RGB image to chosen color space
+I = rgb2hsv(RGB);
+
+% Define thresholds for channel 1 based on histogram settings
+channel1Min = 0.397;
+channel1Max = 0.500;
+
+% Define thresholds for channel 2 based on histogram settings
+channel2Min = 0.543;
+channel2Max = 1.000;
+
+% Define thresholds for channel 3 based on histogram settings
+channel3Min = 0.000;
+channel3Max = 1.000;
+
+% Create mask based on chosen histogram thresholds
+sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
+    (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
+    (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
+BW = sliderBW;
+
+
+%%
 
 %BW_gray = rgb2gray(BW);
 %cornerPoints = detectHarrisFeatures(BW,'MinQuality', 0.2); -> Not used to calculate centriod
