aa5d41e25dc487884acfb820285802659005484d
CamSarkis <cameronsarkis@live.com>
2022-05-08 21:21:14 +1000

    Update

--- matrix_calc.m
+++ matrix_calc.m
@@ -8,36 +8,43 @@ T1 = inv(T1);
 %% Tranformations from cubes to end-effector
 
 %Tranformation from the floor of the table to the camera
+
+
 ThetaX = -180;
 ThetaZ = 90;
+Depth = 0.2;
+CubeCentreX = 5; 
+CubeCentreY = 5; 
 
 
-RotationX_CamToTable = [1 0 0 0; 
-                      0 cos(ThetaX) -sin(ThetaX) 0;
-                      0 sin(ThetaX) cos(ThetaX) 0;
-                      0 0 0 1];
+RotationX_CamToTable = [1 0 0; 
+                      0 cos(ThetaX) -sin(ThetaX);
+                      0 sin(ThetaX) cos(ThetaX)];
+                      
                   
-TranslationX_CamToTable = [1 0 0 CubeCentreX;
-                           0 1 0 0;
-                           0 0 1 0;
-                           0 0 0 1];
+TranslationX_CamToTable = [CubeCentreX;
+                           0;
+                           0];
+                           
                                                  
 % TranslationY_CamToTable = [1 0 0 0;
 %                            0 1 0 0;
 %                            0 0 1 0;
 %                            0 0 0 1];                 
                   
-RotationZ_CamToTable = [cos(ThetaZ) -sin(ThetaZ) 0 0;
-                        sin(ThetaZ) cos(ThetaZ) 0 0;
-                        0 0 1 0;
-                        0 0 0 1];
+RotationZ_CamToTable = [cos(ThetaZ) -sin(ThetaZ) 0];
+                        sin(ThetaZ) cos(ThetaZ) 0];
+                        0 0 1];
+                        
                     
-TranslationZ_CamToTable = [1 0 0 0;
-                           0 1 0 0;
-                           0 0 1 Depth;
-                           0 0 0 1];
+TranslationZ_CamToTable = [0;
+                           0;
+                           Depth];
+                           
 
-Transformation_CamToTable = [
+Rotation_CamToTable = TranslationX_CamToTable*TranslationZ_CamToTable;
+Translation_CamToTable = TranslationX_CamToTable*Depth + 0;
+                         
                   
                   
 
